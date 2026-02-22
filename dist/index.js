3b9cc5dc44b0d681264778297dbe964db41c503d
NullarAI <dev@nullar.ai>
2026-02-22 21:46:45 +0000

    Increase response tokens to 16000 for more findings

--- dist/index.js
+++ dist/index.js
@@ -12799,16 +12799,16 @@ class TokenLimits {
         this.knowledgeCutOff = '2024-01-01';
         if (model === 'MiniMax-M2.5') {
             this.maxTokens = 200000;
-            this.responseTokens = 8000;
+            this.responseTokens = 16000; // Increased to allow more findings
         }
         else if (model === 'GLM-4.7' || model === 'glm-4.7') {
             this.maxTokens = 200000;
-            this.responseTokens = 8000;
+            this.responseTokens = 16000;
         }
         else {
             // Default to MiniMax-M2.5 limits for unknown models
             this.maxTokens = 200000;
-            this.responseTokens = 8000;
+            this.responseTokens = 16000;
         }
         // provide some margin for the request tokens
         this.requestTokens = this.maxTokens - this.responseTokens - 100;
